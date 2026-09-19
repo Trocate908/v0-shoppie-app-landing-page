7c03aaa8bbf4b713828b6a8f2d5f14c3751a8a4f
Trocate908 <miltonmukundwa6@gmail.com>
2026-09-19 23:13:32 +0200

    Update the support/contact phone number to 0789648840 in the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -107,8 +107,8 @@ export default function ContactPage() {
                       </div>
                       <div className="flex-1">
                         <h5 className="font-semibold text-foreground">Call / SMS</h5>
-                        <a href="tel:+263787795039" className="text-sm text-primary hover:underline">
-                          +263 78 779 5039
+                        <a href="tel:+263789648840" className="text-sm text-primary hover:underline">
+                          +263 78 964 8840
                         </a>
                       </div>
                     </div>
